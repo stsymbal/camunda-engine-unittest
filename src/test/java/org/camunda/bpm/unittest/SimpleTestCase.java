c3a71e053e2a4965c21b8755944c3c1e68d0d630
stsymbal <stsymbal@bp-3.com>
2016-04-19 15:56:28 -0500

    Updated SimpleTextCase

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -16,6 +16,8 @@ import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 import org.junit.Rule;
@@ -31,19 +33,19 @@ public class SimpleTestCase {
   public ProcessEngineRule rule = new ProcessEngineRule();
 
   @Test
-  @Deployment(resources = {"testProcess.bpmn"})
+  @Deployment(resources = {"topPMIProcess.bpmn", "testProcess.bpmn"})
   public void shouldExecuteProcess() {
     // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("topPMIProcess");
     // Then it should be active
     assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
-
-    // When we complete that task
-    complete(task(processInstance));
+    // And it should be 3 instances (1 for top process and 2 for called process)
+    assertThat(processInstanceQuery().count()).isEqualTo(3);
+    
+    // When we complete that task of testProcess #1
+    complete(task(processInstanceQuery().processDefinitionKey("testProcess").list().get(0)));
+    // When we complete that task of testProcess #2
+    complete(task(processInstanceQuery().processDefinitionKey("testProcess").list().get(0)));
     // Then the process instance should be ended
     assertThat(processInstance).isEnded();
   }
